ccbaaf8ddde01212816ad8d2d4c05f3226eb555c
Niteen <niteen.java@gmail.com>
2021-04-07 23:27:42 +0530

    Sort 0's,1's and 2's

--- src/dream/SortNumbers.java
+++ src/dream/SortNumbers.java
@@ -2,36 +2,80 @@ package dream;
 
 import java.util.Arrays;
 
+/**
+ * sort a array containing 0, 1 and 2
+ */
 public class SortNumbers {
-    //sort a array containing 0, 1 and 2
-//three-way Partitioning
-    public static void Sort012(int[] array, int end) {
-        int start = 0, mid = 0, pivot = 1;
-
-        while (mid <= end) {
-            if (array[mid] < pivot) { //current element is 0
-                swap(array, start, mid);
-                ++start;
-                ++mid;
-            } else if (array[mid] > pivot) { ////current element is 2
-                swap(array, mid, end);
-                --end;
-            } else   //current element is 1
-                ++mid;
-        }
-    }
-
-    //Function to swap two elements array[i] and array[j] in the array
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
     public static void main(String[] args) {
         int[] array = {2, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
 
         Sort012(array, array.length - 1);
         System.out.println(Arrays.toString(array));
+
+        sort2(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void Sort012(int[] inputArray, int end) {
+        int low = 0, mid = 0, high = inputArray.length - 1;
+
+        //Until mid doesn't cross high
+        while (mid <= high) {
+            //If an element is 0, bring it to the beginning
+            if (inputArray[mid] == 0) {
+                swap(inputArray, low, mid);
+                low++;
+                mid++;
+            } else if (inputArray[mid] == 2) {
+                swap(inputArray, mid, high);
+                high--;
+            } else {
+                mid++; //If an element is 1, let it be there only
+            }
+        }
+    }
+
+    private static void swap(int[] inputArray, int i, int j) {
+        int temp = inputArray[i];
+        inputArray[i] = inputArray[j];
+        inputArray[j] = temp;
+    }
+
+    public static void sort2(int[] arr) {
+        int c1 = 0, c2 = 0, c3 = 0;
+
+        for (int i : arr) {
+            switch (i) {
+                case 0: {
+                    c1++;
+                    break;
+                }
+                case 1: {
+                    c2++;
+                    break;
+                }
+                case 2: {
+                    c3++;
+                    break;
+                }
+            }
+        }
+
+        int i = 0;
+
+        while (c1 > 0) {
+            arr[i++] = 0;
+            c1--;
+        }
+
+        while (c2 > 0) {
+            arr[i++] = 1;
+            c2--;
+        }
+
+        while (c3 > 0) {
+            arr[i++] = 2;
+            c3--;
+        }
     }
 }
